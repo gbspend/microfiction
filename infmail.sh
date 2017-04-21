26a0970b0bce997ae35548d0da24c042ce5d3394
gbspend <brad.spendlove@gmail.com>
2017-04-21 12:49:25 -0600

    removed gmail password

--- infmail.sh
+++ infmail.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#need a real gmail uname and password
 
-while true; do python multimicro.py sad_JJ 5 | sendemail -f "bradcompscimail@gmail.com" -t "lessthan1337@gmail.com" -u "SWS Report" -o tls=yes -o timeout=0 -s smtp.gmail.com:587 -xu bradcompscimail@gmail.com -xp byucompsci; sleep 1; done
+while true; do python multimicro.py sad_JJ 5 | sendemail -f "bradcompscimail@gmail.com" -t "lessthan1337@gmail.com" -u "SWS Report" -o tls=yes -o timeout=0 -s smtp.gmail.com:587 -xu bradcompscimail@gmail.com -xp REDACTED; sleep 1; done
