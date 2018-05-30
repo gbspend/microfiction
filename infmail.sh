e947a9453055ca19c3497955a97d1dd4380aa86d
gbspend <brad.spendlove@gmail.com>
2018-05-30 19:48:10 -0400

    added linux commands to run/kill in background

--- infmail.sh
+++ infmail.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #need a real gmail uname and password
+#run in bg with: nohup ./infmail.sh > /dev/null 2>&1 &
+#see bg jobs with: ps aux
 
 while true; do python newmicro.py 5 | sendemail -f "bradcompscimail@gmail.com" -t "lessthan1337@gmail.com" -u "SWS Report" -o tls=yes -o timeout=0 -s smtp.gmail.com:587 -xu bradcompscimail@gmail.com -xp REDACTED; sleep 1; done
