be3108028aaecb31eede4fdd9d3a586f1d9ca79d
brad <brad.spendlove@.com>
2018-05-31 11:52:01 -0600

    redirected error to email

--- infmail.sh
+++ infmail.sh
@@ -3,4 +3,4 @@
 #run in bg with: nohup ./infmail.sh > /dev/null 2>&1 &
 #see bg jobs with: ps aux
 
-while true; do python newmicro.py 5 | sendemail -f "bradcompscimail@gmail.com" -t "lessthan1337@gmail.com" -u "SWS Report" -o tls=yes -o timeout=0 -s smtp.gmail.com:587 -xu bradcompscimail@gmail.com -xp REDACTED; sleep 1; done
+while true; do python newmicro.py 5 2>&1 | sendemail -f "bradcompscimail@gmail.com" -t "lessthan1337@gmail.com" -u "SWS Report" -o tls=yes -o timeout=0 -s smtp.gmail.com:587 -xu bradcompscimail@gmail.com -xp REDACTED; sleep 1; done
