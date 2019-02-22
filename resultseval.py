8ee23be53fa5e2d03a22f45468ecb040962da4d2
Brad Spendlove <brad.spendlove@gmail.com>
2019-02-21 20:31:38 -0700

    cleaned up survey results eval

--- resultseval.py
+++ resultseval.py
@@ -49,9 +49,114 @@ for s in stories:
 	cohsc.append(avg(coh[s]))
 	impsc.append(avg(imp[s]))
 
-plt.plot(scorersc)
-plt.plot(cohsc)
-plt.plot(impsc)
+
+
+
+
+#============
+import numpy as np
+import matplotlib.pyplot as plt
+
+def estimate_coef(x, y):
+	# number of observations/points
+	n = np.size(x)
+
+	# mean of x and y vector
+	m_x, m_y = np.mean(x), np.mean(y)
+
+	# calculating cross-deviation and deviation about x
+	SS_xy = np.sum(y*x) - n*m_y*m_x
+	SS_xx = np.sum(x*x) - n*m_x*m_x
+
+	# calculating regression coefficients
+	b_1 = SS_xy / SS_xx
+	b_0 = m_y - b_1*m_x
+
+	return(b_0, b_1)
+
+def plot_regression_line(x, y, b,lbl):
+	# predicted response vector
+	y_pred = b[0] + b[1]*x
+
+	# plotting the regression line
+	plt.plot(x, y_pred, label=lbl)
+
+def plot_regression(x,y,label=''):
+	x = np.array(x)
+	y = np.array(y)
+	# estimating coefficients
+	b = estimate_coef(x, y)
+	# plotting regression line
+	plot_regression_line(x, y, b, label)
+
+#============
+
+
+
+
+
+
+
+
+
+
+#plt.plot(scorersc) #messy
+plt.plot(cohsc,'o',label='Average coherence')
+plot_regression(list(range(115)),cohsc,label='Coherence Regression')
+plt.plot(impsc,'o',label='Average impact')
+plot_regression(list(range(115)),impsc,label='Impact Regression')
+plt.legend()
+plt.title("Average human-rated coherence and impact of stories sorted by skipthought score (descending)")
 plt.show()
 
 
+cohgood = []
+cohbad = []
+impgood = []
+impbad = []
+
+for i in range(100):
+	s=stories[i]
+	cohgood+=coh[s]
+	impgood+=imp[s]
+
+for i in range(100,115):
+	s=stories[i]
+	cohbad+=coh[s]
+	impbad+=imp[s]
+
+plt.boxplot([cohgood,cohbad])
+plt.xticks([1,2], ('Top 100 skipthought-scored stories', 'Bottom 15'))
+plt.title("Boxplot comparison of human-scored coherence of top and bottom skipthought-scored stories")
+plt.show()
+plt.boxplot([impgood,impbad])
+plt.xticks([1,2], ('Top 100 skipthought-scored stories', 'Bottom 15'))
+plt.title("Boxplot comparison of human-scored impact of top and bottom skipthought-scored stories")
+plt.show()
+
+
+from scipy.stats import ttest_ind
+def printstatp(stat,p):
+	ps = '%.3f'%p
+	print "ps:",ps
+	if ps == "0.000":
+		ps = "p<0.001"
+	else:
+		ps = "p="+ps
+	print('Statistics=%.3f, %s' % (stat, ps))
+
+# compare samples
+def comparesamp(good,bad,alpha=0.05):
+	stat, p = ttest_ind(good, bad)
+	printstatp(stat, p)
+	#p < .001
+	# interpret
+	if p > alpha:
+		print('Same distributions (fail to reject H0)')
+	else:
+		print('Different distributions (reject H0)')
+
+print "Coherence:"
+comparesamp(cohgood,cohbad)
+print "Impact:"
+comparesamp(impgood,impbad)
